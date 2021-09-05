b81f7dfb1ffd93ab4364ce96c083f3e7eae3854a
杨阳 <1002522146@qq.com>
2021-09-05 23:32:22 +0800

    :sparkles: 加一点功能

--- dist/cli.js
+++ dist/cli.js
@@ -4,12 +4,44 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 exports.__esModule = true;
+exports.handleError = exports.printUnknownCommand = void 0;
 var react_1 = __importDefault(require("react"));
 var ink_1 = require("ink");
 var creat_1 = __importDefault(require("./creat"));
 var help_1 = __importDefault(require("./help"));
 var commander_1 = require("commander");
 var program = new commander_1.Command();
+function printUnknownCommand(cmdName) {
+    var errorMsg = "\u547D\u4EE4 \"" + chalk.bold(cmdName) + "\" \u4E0D\u5B58\u5728";
+    if (cmdName) {
+        console.log('运行 ${chalk.bold(', "ks --help", ')} 查看可用命令');
+    }
+    else {
+        program.outputHelp();
+    }
+}
+exports.printUnknownCommand = printUnknownCommand;
+function handleError(err) {
+    if (err) {
+        // Some error messages (esp. custom ones) might have `.` at the end already.
+        console.log("Run CLI with $");
+        // if (err.stack) {
+        //     console.log(err.stack);
+        // }
+    }
+    process.exit();
+}
+exports.handleError = handleError;
+program.usage('[command] [command-options]')
+    .exitOverride(handleError)
+    .action(function (cmd) {
+    // if(![
+    // 'c','t'
+    // ].includes(cmd)){
+    //   console.log('gundan');
+    //   process.exit();
+    // }
+});
 program
     .command('create <app-name>')
     .alias('c')
@@ -32,5 +64,5 @@ program
     // 新增说明信息
     ink_1.render(react_1["default"].createElement(help_1["default"], null));
 });
-program.version('0.0.1', '-v', 'output the current version');
+// program.version('0.0.1','-v','output the current version')
 program.parse(process.argv);
--- src/cli.tsx
+++ src/cli.tsx
@@ -6,7 +6,42 @@ import Help from "./help";
 import { Command } from "commander";
 
 const program = new Command();
+export function printUnknownCommand(cmdName: string) {
+  const errorMsg = `命令 "${chalk.bold(cmdName)}" 不存在`;
 
+  if (cmdName) {
+     console.log('运行 ${chalk.bold('"ks --help"')} 查看可用命令');
+     
+  } else {
+      program.outputHelp();
+  }
+}
+export function handleError(err: Error | null | undefined) {
+  if (err) {
+      // Some error messages (esp. custom ones) might have `.` at the end already.
+      console.log(  `Run CLI with $`,);
+      
+      // if (err.stack) {
+      //     console.log(err.stack);
+          
+      // }
+  }
+  process.exit();
+}
+
+
+program.usage('[command] [command-options]')
+.exitOverride(handleError)
+.action((cmd)=>{
+  // if(![
+  // 'c','t'
+  // ].includes(cmd)){
+  //   console.log('gundan');
+    
+  //   process.exit();
+  // }
+ 
+})
 
 program
 .command('create <app-name>')
@@ -31,5 +66,5 @@ program
     render(<Help />);
   })
 
-program.version('0.0.1','-v','output the current version')
+// program.version('0.0.1','-v','output the current version')
 program.parse(process.argv);
